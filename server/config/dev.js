2c6a8575e0a48940b20ac828b1aa3ef8c0b23c79
Clairson Patrick Bosi <cpbosi@gmail.com>
2020-11-27 21:38:12 -0300

    recuperando projeto note antigo

---
+++ server/config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+	googleClientID: '898981639890-v5kqc018g3kmilj0q4qqqr27k61lmhri.apps.googleusercontent.com',
+	googleClientSecret: 'gS3R1UuGs4qj4uIz6nqA1kCQ',
+	mongoURI: 'mongodb://patrick:password@ds119088.mlab.com:19088/emaily-dev',
+	cookieKey: 'asdfasdfasdfasdfaasdfasdfasdfadsfasdf',
+	stripePublishableKey: 'pk_test_QGMwlnfgEmEtomCscHhaWj20',
+	stripeSecretKey: 'sk_test_9GD6DznWP6lyAD7voSg0jAop',
+	sendGridKey: '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',
+	redirectDomain: 'http://localhost:3000'
+};
---
+++ server/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const keys = require("./config/keys");
+require("./models/User");
+require("./models/Survey");
+require("./services/passport");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const bodyParser = require('body-parser');
+const authRoutes = require("./routes/authRoutes");
+
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cookieSession({
+		maxAge: 30 * 24 * 60 * 60 *1000,
+		keys: [keys.cookieKey]
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+//authRoutes(app);
+require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
+
+if(process.env.NODE_ENV == 'production'){
+	//se vir no padrão abaixo retorna o arquivo dentro da pasta build
+	app.use(express.static('client/build'));
+
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
